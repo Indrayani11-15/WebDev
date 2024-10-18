bbffac4ce894895c85a987a5fd4b4f87ce483357
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-10-18 13:57:39 +0530

    part 1 basics js

---
+++ JSFOUNDATION/part1/logical.js
@@ -0,0 +1,14 @@
+//&& -> and
+//|| -> or
+// ! -> reverse
+
+let isLoggedIn = true
+let ispaid = false
+
+console.log(isLoggedIn && ispaid);//false
+
+let isEmailUser = true
+let isGoogelUser = false
+
+console.log(isEmailUser || isGoogelUser);//true
+
