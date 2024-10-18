6b23378ec7f232ee341e266aafb9cbecfa685bfc
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-10-18 13:44:06 +0530

    Basic into to JS

---
+++ JSFOUNDATION/part1/changes.js
@@ -0,0 +1,12 @@
+let gameName = "spiderman"
+
+gameName = "batman"
+
+console.log(gameName);
+
+const userName = "hiteshdotcom";
+
+userName = "hitest";
+
+console.log(userName);
+
---
+++ JSFOUNDATION/part1/datatype.js
@@ -0,0 +1,31 @@
+console.log("HEllo");
+/*
+String
+Number
+Boolean
+Bigint
+
+
+undefined
+null
+
+object
+
+Symbol
+
+
+*/
+
+// var score = 102
+
+let score = 102;
+let name = "Chaicode.com"
+let isLoggedIn = false
+
+//object
+let teaTypes = ["lemontea", "orangetea", "oolong tea"]
+let user = {firstname:"Indrayani", lastname:"Verulkar"}
+
+let getScore = score;//we can borrow value from other variables
+
+console.log(getScore);
