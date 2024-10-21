4488a6888ec2ee366918d6acfb97978b053e310a
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-10-21 10:37:58 +0530

    objects and arrays and conversion

---
+++ JSFOUNDATION/part2/nonprimitive.js
@@ -0,0 +1,26 @@
+//Objects
+
+const username = {
+    firstname:"Hitesh",
+    isLoggedin: true,
+};
+
+// username.firstname = "Mr.H"
+// username.isLoggedin = false
+// username.lastname = "verulkar"
+
+// console.log (username);
+// console.log(typeof username);
+
+let today = new Date()
+// console.log(today.getDate());
+
+
+//Array
+let anotherUser = ["hitesh", true];
+// console.log(anotherUser[0]);
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
+
