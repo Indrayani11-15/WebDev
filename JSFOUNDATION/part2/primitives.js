a513341c44a730f798f0780323ed9d83071bff8f
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-10-21 10:21:23 +0530

    primitive data types

---
+++ JSFOUNDATION/part2/primitives.js
@@ -0,0 +1,44 @@
+//Number
+
+let balance = 120;
+let anotherBalance = new Number(120)
+
+// console.log(balance);
+// console.log(anotherBalance.valueOf());
+
+// console.log(typeof(balance));
+// console.log(typeof(anotherBalance));
+
+//boolean
+let isActive = true
+let isReallyActive = new Boolean(true); //not recommended
+
+//null and undefined
+
+let firstname = null;
+let lastname = undefined;
+// console.log(firstname);
+// console.log(lastname);
+
+//String
+
+let myString = "Hello"
+let myStringOne = 'Hola'
+let username = 'Indrayani'
+
+let oldGreet = myString + " Hitesh"
+console.log(oldGreet);
+
+let greetMsg = `Hello ${username} !`
+let demoOne = `Value is ${2*78}`
+console.log(greetMsg);
+console.log(demoOne);
+
+//symbol
+
+let sm1 = Symbol()
+let sm2 = Symbol()
+
+ 
+
+
