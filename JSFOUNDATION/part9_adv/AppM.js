95fd362e3e40555346567ad5aa9c231ae2b5685e
Indrayani11-15 <verulkarindrayani@gmail.com>
2024-11-13 21:09:46 +0530

    importing and exporting modules

---
+++ JSFOUNDATION/part9_adv/AppM.js
@@ -0,0 +1,9 @@
+//modules import
+
+//default import
+
+import multiply from "./mathOperationsM";
+
+//named import
+
+import{add,subtract} from "./mathOperationsM"
---
+++ JSFOUNDATION/part9_adv/mathOperationsM.js
@@ -0,0 +1,12 @@
+export function add(){
+    return a + b
+}
+
+export function subtract(){
+    return a-b
+
+}
+
+export default function multiply(a,b){
+    return a*b
+}
